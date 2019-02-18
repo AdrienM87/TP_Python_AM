b8a70c5aa8892e8c40bb44eb7cf7536e5636b727
ADRIEN MARINI <adrien.marini@viacesi.fr>
2019-02-18 12:04:56 +0100

    affichage de variables bis

--- 1.py
+++ 1.py
@@ -12,9 +12,24 @@ ligne 3"""
 ########################
 
 
-#Affiche sur la console le contenu du print
+#Assignation de variables
 a = b = c = 1
+
+#Affiche sur la console le contenu du print
 print(a)
 print(b)
 print(c)
+
+#Assignation de variables
+chaine = "hello world"
+nombre = 12345
+counter = 100 # An integer assignment
+miles = 1000.0 # A floating point
+name = "John" # A string print counter
+
+#Affichage du contenu des variables
+print(miles)
+print(counter)
+print(name)
+
 
