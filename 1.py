03ad02257faf653205d1f8fb285338d80dbc4523
ADRIEN MARINI <adrien.marini@viacesi.fr>
2019-02-18 12:04:03 +0100

    affichage console avec variables

--- 1.py
+++ 1.py
@@ -1,6 +1,20 @@
+"""
+Cour Python - Notebook 1
+"""
+
 """Doc
 ligne 1
 ligne 2
 ligne 3"""
 
 #doc sur une ligne
+
+########################
+
+
+#Affiche sur la console le contenu du print
+a = b = c = 1
+print(a)
+print(b)
+print(c)
+
